401758586bec7c5ba9aa4aea164abd00feafdf0f
21522193@gm.uit.edu.vn <khangnguyen48@gmail.com>
2023-11-17 23:20:15 +0700

    feat: add LandingPage.jsx file

---
+++ cdm-ui/src/pages/LandingPage/LandingPage.jsx
@@ -0,0 +1,21 @@
+import NavBar from "./NavBar";
+import Hero from "./Hero"
+import Subcribe from "./Subcribe";
+import Footer from "./Footer";
+import About from "./About";
+
+
+function LandingPage() {
+  return (
+    <div >
+      <NavBar />
+      <Hero />
+      <About />
+      <Subcribe />
+      <Footer />
+      
+    </div>
+  );
+}
+
+export default LandingPage;
