53408eabe1f5e43b97d7818017cf8d104bc58873
Nguyễn Hoàng Minh <109229679+hoangmindrespect@users.noreply.github.com>
2023-12-21 11:59:56 +0700

    feat: feature create report

--- cdm-ui/src/pages/Customer/CustomerReport/index.jsx
+++ cdm-ui/src/pages/Customer/CustomerReport/index.jsx
@@ -1,22 +1,48 @@
 import SideBar from "../../../layouts/components/SideBar";
 import "./CustomerReport.css";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useRef } from "react";
 import { cdmApi } from "../../../misc/cdmApi";
 import { Checkbox, Navbar } from "@material-tailwind/react";
 import ImageUploading from 'react-images-uploading';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPenToSquare, faRectangleXmark, faUpload, faXmark } from "@fortawesome/free-solid-svg-icons";
+import { Check, CheckRounded } from "@mui/icons-material";
+import axios, { Axios } from "axios";
+import DragAndDrop from "../../../components/DragAndDrop";
+import { Snackbar } from "@mui/material";
+import Alert from "@mui/material/Alert";
+
 
 function CustomerReport() {
+  const [snackbar, setSnackbar] = React.useState(null);
+  const handleCloseSnackbar = () => setSnackbar(null);
+  const [selectedOption, setSelectedOption] = useState(null);
   const [title, setTitle] = useState("");
-    const [description, setDescription] = useState("");
-    const [image, setImage] = useState("");
-    const [userId, setUserId] = useState("");
-    const [file, setFile] = useState("");
-    const [imagePreviewUrl, setImagePreviewUrl] = useState("");
-    const fileInput = useRef(null)
-    const user = JSON.parse(localStorage.getItem("currentUser"));
+  const [description, setDescription] = useState("");
+  const [userId, setUserId] = useState("");
+  const [file, setFile] = useState("");
+  const [imagePreviewUrl, setImagePreviewUrl] = useState("");
+  const fileInput = useRef(null)
+  const user = JSON.parse(localStorage.getItem("currentUser"));
+
+  let img = "";
+  const uploadImage = async (thing) => {
+    const image = new FormData()
+
+    image.append("file", thing)
+    image. append("cloud_name", "droondbdu")
+    image. append("upload_preset", "cdmpreset")
+    
+    const response = await fetch("https://api.cloudinary.com/v1_1/droondbdu/image/upload", 
+        {
+          method: "post",
+          body: image
+        }
+    )
+    const imgData = await response.json();
+    img = imgData.url.toString();
+  };
 
   const handleChange = (event) => {
     setReport({ ...report, [event.target.name]: event.target.value });
@@ -24,11 +50,18 @@ function CustomerReport() {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
+    //uploadImage(img);
     const userId = user.id;
-    const report = { title, description, userId, image, type: "USER" };
+    const report = { title, description, userId, image: img, type: "USER" };
+    if(title == "" || description == "")
+    {
+      setSnackbar({ children: "Plese describe your problem", severity: "error" });
+      return;
+    }
     await cdmApi.createCustomerReport(report)
       .then((response) => {
-        alert("Report successfully!");
+        setSnackbar({ children: "Your report is sent", severity: "success" });
+        window.location.reload() 
         console.log(response.data);
       })
       .catch((error) => {
@@ -36,13 +69,22 @@ function CustomerReport() {
       });
   };
 
-  const [images, setImages] = React.useState([]);
-  const maxNumber = 4;
-
-  const onChange = (imageList, addUpdateIndex) => {
-    // data for submit
-    console.log(imageList, addUpdateIndex);
-    setImages(imageList);
+  const handleRadioChange = (value) => {
+    setSelectedOption(value);
+    switch (value) {
+      case 'website':
+        setTitle("Website Problem");
+        break;
+      case 'service':
+        setTitle("Service Problem");
+        break;
+      case 'product':
+        setTitle("Product Problem");
+        break;
+      default:
+        setTitle("");
+        break;
+    }
   };
 
   return (
@@ -50,62 +92,80 @@ function CustomerReport() {
         <div className="flex">
             <SideBar />
             <div style={{ marginLeft: 40, width: "100vw" }}>
-                  <h1 className="font-medium text-3xl mt-16">Profile Settings</h1>
+                  <h1 className="font-medium text-3xl mt-16">Report Problem</h1>
                   <p className="mt-8 xl:mr-2 mr-4">Which of the following options best describes the type of issue you are experiencing</p>
                   <div className="flex flex-col xl:ml-8 ml-2">
-                        <div className="flex"><Checkbox color="blue" defaultChecked /><label className="mt-2.5" htmlFor="">Website Problem</label></div>
-                        <div className="flex"><Checkbox color="blue" defaultChecked /><label className="mt-2.5" htmlFor="">Service Problem</label></div>
-                        <div className="flex"><Checkbox color="blue" defaultChecked /><label className="mt-2.5" htmlFor="">Product's Problem</label></div>
-
+                      <div class="inline-flex items-center">
+                          <label class="relative flex items-center p-3 rounded-full cursor-pointer" htmlFor="html">
+                            <input name="type" type="radio" 
+                              class="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-full border border-blue-gray-200 text-gray-900 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-gray-900 checked:before:bg-gray-900 hover:before:opacity-10"
+                              id="html" onChange={() => handleRadioChange('website')}
+                              checked={selectedOption === 'website'}/>
+                            <span
+                              class="absolute text-gray-900 transition-opacity opacity-0 pointer-events-none top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 peer-checked:opacity-100">
+                              <svg xmlns="http://www.w3.org/2000/svg" class="h-3.5 w-3.5" viewBox="0 0 16 16" fill="currentColor">
+                                <circle data-name="ellipse" cx="8" cy="8" r="8"></circle>
+                              </svg>
+                            </span>
+                          </label>
+                          <label class="mt-px font-light text-gray-700 cursor-pointer select-none" htmlFor="html">
+                            Website Problem
+                          </label>
+                        </div>
+                        <div class="inline-flex items-center">
+                          <label class="relative flex items-center p-3 rounded-full cursor-pointer" htmlFor="html">
+                            <input name="type" type="radio" 
+                              class="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-full border border-blue-gray-200 text-gray-900 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-gray-900 checked:before:bg-gray-900 hover:before:opacity-10"
+                              id="html" onChange={() => handleRadioChange('service')}
+                              checked={selectedOption === 'service'}/>
+                            <span
+                              class="absolute text-gray-900 transition-opacity opacity-0 pointer-events-none top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 peer-checked:opacity-100">
+                              <svg xmlns="http://www.w3.org/2000/svg" class="h-3.5 w-3.5" viewBox="0 0 16 16" fill="currentColor">
+                                <circle data-name="ellipse" cx="8" cy="8" r="8"></circle>
+                              </svg>
+                            </span>
+                          </label>
+                          <label class="mt-px font-light text-gray-700 cursor-pointer select-none" htmlFor="html">
+                            Service Problem
+                          </label>
+                        </div>
+                        <div class="inline-flex items-center">
+                          <label class="relative flex items-center p-3 rounded-full cursor-pointer" htmlFor="react">
+                            <input name="type" type="radio" 
+                              class="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-full border border-blue-gray-200 text-gray-900 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-gray-900 checked:before:bg-gray-900 hover:before:opacity-10"
+                              id="react" onChange={() => handleRadioChange('product')}
+                              checked={selectedOption === 'product'}/>
+                            <span
+                              class="absolute text-gray-900 transition-opacity opacity-0 pointer-events-none top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 peer-checked:opacity-100">
+                              <svg xmlns="http://www.w3.org/2000/svg" class="h-3.5 w-3.5" viewBox="0 0 16 16" fill="currentColor">
+                                <circle data-name="ellipse" cx="8" cy="8" r="8"></circle>
+                              </svg>
+                            </span>
+                          </label>
+                          <label class="mt-px font-light text-gray-700 cursor-pointer select-none" htmlFor="react">
+                            Product Problem
+                          </label>
+                        </div>
                   </div>
                   <p className="mt-4 xl:mr-2 mr-4">Please describe the problem you are experiencing in the space below. Be as descriptive as possible so we can be sure to help you as best as we can.</p>
-                  <textarea  rows="4" className="mt-4 mr-2 xl:mr-24 block p-2.5 w-4/5 text-sm text-black rounded-lg border border-gray-700 focus:border-black bg-white" placeholder="Write your thoughts here..."></textarea>
+                  <textarea onChange={(e) => setDescription(e.target.value)} rows="5" className="p-6 mt-4 mr-2 xl:mr-24 block p-2.5 w-4/5 text-sm text-black rounded-lg border border-gray-200 focus:border-black bg-white" placeholder="Write your thoughts here..."></textarea>
                   <p className="mt-4 xl:mr-2 mr-4">Attach Image (if neccessary)</p>
-                  <ImageUploading multiple value={images} onChange={onChange} maxNumber={maxNumber} dataURLKey="data_url">
-                      {({
-                        imageList,
-                        onImageUpload,
-                        onImageRemoveAll,
-                        onImageUpdate,
-                        onImageRemove,
-                        isDragging,
-                        dragProps,
-                      }) => (
-                        <div className="upload__image-wrapper mt-2">
-                          <button className="border-solid border-2 border-black hover:bg-lime-200"
-                            style={isDragging ? { color: 'red' } : undefined}
-                            onClick={onImageUpload}
-                            {...dragProps}
-                          >
-                            <FontAwesomeIcon icon={faUpload}/>
-                          </button>
-                          &nbsp;
-                          <button className="border-solid border-2 border-black px-3 hover:bg-red-200" onClick={onImageRemoveAll}><FontAwesomeIcon icon={faXmark}/></button>
-                          <div className="flex xl:mr-0 mr-2" style={{marginLeft: -8}}>
-                              {imageList.map((image, index) => (
-                                  <div key={index} className="image-item mt-2 ml-2">
-                                        <img src={image['data_url']} alt="" width="100" />
-                                        <div className="image-item__btn-wrapper ml-2 ">
-                                          <button onClick={() => onImageUpdate(index)}><FontAwesomeIcon icon={faPenToSquare}/></button>
-                                          <button onClick={() => onImageRemove(index)}><FontAwesomeIcon icon={faRectangleXmark}/></button>
-                                        </div>
-                                  </div>
-                                ))}
-                          </div>
-                        </div>
-                      )}
-                    </ImageUploading>
-                    <p className="mt-4 xl:mr-2 mr-4">How would you like us to contact you? Please select an option from the list below.</p>
-                    <div className="flex flex-col xl:ml-8 ml-2">
+                  <DragAndDrop uploadImage={uploadImage}/>
+                  <p className="mt-4 xl:mr-2 mr-4">How would you like us to contact you? Please select an option from the list below.</p>
+                  <div className="flex flex-col xl:ml-8 ml-2">
                         <div className="flex"><Checkbox color="blue" defaultChecked /><label className="mt-2.5" htmlFor="">Phone Call</label></div>
                         <div className="flex"><Checkbox color="blue" defaultChecked /><label className="mt-2.5" htmlFor="">Text Message</label></div>
                         <div className="flex"><Checkbox color="blue" defaultChecked /><label className="mt-2.5" htmlFor="">Email Address</label></div>
                   </div>
-
-                  <button class="mt-2 mb-64 bg-black hover:bg-gray-700 text-white font-bold py-2 px-4 rounded text-center mr-2">
+                  <button onClick={handleSubmit} class="mt-2 mb-64 bg-black hover:bg-gray-700 text-white font-bold py-2 px-4 rounded text-center mr-2">
                         Submit
                   </button>
-            </div>
+                 </div>
+                 {!!snackbar && (
+              <Snackbar open onClose={handleCloseSnackbar} autoHideDuration={6000} anchorOrigin={{ vertical: "bottom", horizontal: "center" }}>
+                <Alert {...snackbar} onClose={handleCloseSnackbar} />
+              </Snackbar>
+            )}
         </div>
     </>
   );
