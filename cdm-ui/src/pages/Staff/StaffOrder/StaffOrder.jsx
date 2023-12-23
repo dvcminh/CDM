dc09dfff4faf9b751e900ddbcf4f5e96d88e3602
Nguyễn Hoàng Minh <109229679+hoangmindrespect@users.noreply.github.com>
2023-12-23 23:40:30 +0700

    fix: staff's profile

--- cdm-ui/src/pages/Staff/StaffOrder/StaffOrder.jsx
+++ cdm-ui/src/pages/Staff/StaffOrder/StaffOrder.jsx
@@ -1,13 +1,12 @@
 import React , { useState } from 'react';
 import SideBarStaff from '../../../layouts/components/SideBarStaff';
 import './StaffOrder.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleXmark } from '@fortawesome/free-solid-svg-icons';
 import OrderDetailModal from '../../Customer/CustomerOrderHistory/component/OderdetailModal'
 import { cdmApi } from '../../../misc/cdmApi';
 import { useEffect } from 'react';
-import RightClickComponent from './MenuContext';
 import MenuDefault from './MenuContext';
+
+
 
 const StaffOrder = () => {
 
@@ -68,28 +67,7 @@ const StaffOrder = () => {
       console.log("Updated productData:", userInfor[0]);
     }, [userInfor]);
 
-    //menu context
-    const [menuVisible, setMenuVisible] = useState(false);
-    const [menuPosition, setMenuPosition] = useState({ top: 0, left: 0 });
-    const [selectedOption, setSelectedOption] = useState(null);
 
-    const handleContextMenu = (e) => {
-      e.preventDefault();
-      setMenuVisible(true);
-      setMenuPosition({ top: e.clientY, left: e.clientX });
-    };
-
-    const handleOptionClick = (option) => {
-      // Handle the selected option here
-      setSelectedOption(option);
-      // Close the menu
-      setMenuVisible(false);
-    };
-
-    const handleOutsideClick = () => {
-      // Close the menu when clicking outside
-      setMenuVisible(false);
-    };
     return(
         <div>
         {modalOpen && <OrderDetailModal data={orderDetail} setOpenModal={setModalOpen} />}
@@ -98,36 +76,45 @@ const StaffOrder = () => {
             <SideBarStaff className='flex-1'/>
             <div className="mt-8 mr-16">
             <h1 className='font-medium text-3xl mt-8 ml-12'>Manage Customer's Orders</h1>
-            <table className='ml-12 mt-8'>
-              <thead>
-                <tr>
-                  <th>No.</th>
-                  <th>Customer's Name</th>
-                  <th>Order Date</th>
-                  <th>Total Amount</th>
-                  <th>Shipping Address</th>
-                  <th>Payment Status</th>
-                  <th>Shipping Status</th>
-                  <th>Detail</th>
-                </tr>
-              </thead>
-              <tbody>
-                {orders.map((order, index) => (
-                  <tr key={order.id}>
-                    <td>{index + 1}</td>
-                    <td>{userInfor[index].email}</td>
-                    <td>{new Date(order.orderDate).toLocaleDateString()}</td>
-                    <td>${order.totalAmount}</td>
-                    <td>{order.shippingAddress}</td>
-                    <td className="text-lime-700"><MenuDefault option={["Waiting", "Paid"]} current={"Waiting"}/></td>
-                    <td className="text-lime-700"><MenuDefault option={["Pending", "Approved", "Reject"]}  current={"Pending"}/></td>
-                    {/* <td>${order.voucherValue}</td>
-                    <td>${order.shippingValue}</td> */}
-                    <td><button onClick={() => getOrdersDetail(order.id)} type="button" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">View</button></td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
+            <table className='ml-12 mt-8 w-full table-auto'>
+                  <thead> 
+                    <tr className='text-sm'>
+                      <th className='py-2'>No.</th>
+                      <th className='py-2'>Name</th>
+                      <th className='py-2'>Order Date</th>
+                      <th className='py-2'>Total</th>
+                      <th className='py-2'>Shipping Address</th>
+                      <th className='py-2'>Payment Status</th>
+                      <th className='py-2'>Shipping Status</th>
+                      <th className='py-2'>Detail</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {orders.map((order, index) => (
+                      <tr key={order.id} className='text-sm'>
+                        <td className='py-2'>{index + 1}</td>
+                        <td className='py-2'>{userInfor[index].email}</td>
+                        <td className='py-2'>{new Date(order.orderDate).toLocaleDateString()}</td>
+                        <td className='py-2'>${order.totalAmount}</td>
+                        <td className='py-2'>{order.shippingAddress}</td>
+                        <td className='py-2 text-lime-700'><MenuDefault option={["Waiting", "Paid"]} current={"Waiting"}/></td>
+                        <td className='py-2 text-lime-700'><MenuDefault option={["Pending", "Approved", "Reject"]}  current={"Pending"}/></td>
+                        {/* <td>${order.voucherValue}</td>
+                        <td>${order.shippingValue}</td> */}
+                        <td className='py-2'>
+                          <button
+                            onClick={() => getOrdersDetail(order.id)}
+                            type="button"
+                            className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+                          >
+                            View
+                          </button>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+
           </div>
         </span>
         ) : (
