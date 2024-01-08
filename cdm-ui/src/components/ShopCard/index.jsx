5f2eae1027131653acdffcb71aac4ab3d652d06c
Nguyễn Hoàng Minh <109229679+hoangmindrespect@users.noreply.github.com>
2024-01-08 14:54:25 +0700

    feat: add product from product detail

--- cdm-ui/src/components/ShopCard/index.jsx
+++ cdm-ui/src/components/ShopCard/index.jsx
@@ -41,15 +41,15 @@ function ShopCard({data}) {
     }
 
     return ( 
-        <div className="relative m-10 w-full max-w-xs overflow-hidden rounded-lg bg-white shadow-md">
+        <div className="relative m-10 w-full max-w-xs overflow-hidden rounded-lg bg-white shadow-md dark:bg-gray-500">
                 <div  className="">
                     <a href="" onClick={viewDetail}>
-                        <img className="h-60 rounded-t-lg object-cover" src={data.imgSrc} alt="product image" />
+                        <div className="bg-white"><img className="h-60 rounded-t-lg object-cover " src={data.imgSrc} alt="product image" /></div>
                     </a>
                     <span className="absolute top-0 left-0 w-28 translate-y-4 -translate-x-6 -rotate-45 bg-black text-center text-sm text-white">Sale</span>
                     <div className="mt-4 px-5 pb-5">
                     <a href="" onClick={viewDetail}>
-                        <h5 className="text-xl font-semibold tracking-tight text-slate-900">{data.name}</h5>
+                        <h5 className="text-xl font-semibold tracking-tight text-slate-900 dark:text-white">{data.name}</h5>
                     </a>
                 </div>
           <div className="mt-2.5 mb-5 flex items-center ml-4">
@@ -72,14 +72,14 @@ function ShopCard({data}) {
           </div>
           <div className="ml-4 flex items-center justify-between">
             <p>
-              <span className="text-3xl font-bold text-slate-900">{data.price}</span>
-              <span className="text-sm text-slate-900 line-through">$299</span>
+              <span className="text-3xl font-bold text-slate-900 dark:text-white">${data.price}</span>
+              <span className="text-sm text-slate-900 line-through dark:text-white">${data.price  + 100}</span>
             </p>
-            <button onClick={() => handleCart(data)}  className="mr-4 mb-2 flex items-center rounded-md bg-slate-900 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300">
+            <button onClick={() => handleCart(data)}  className="dark:bg-blue-500 dark:hover:bg-blue-700 mr-4 mb-2 flex items-center rounded-md bg-slate-900 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300">
               <svg xmlns="http://www.w3.org/2000/svg" className="mr-2 h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                 <path strokeLinecap="round" strokeLinejoin="round" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
               </svg>
-              Add to cart</button>
+              Add</button>
           </div>
         </div>
         {!!snackbar && (
